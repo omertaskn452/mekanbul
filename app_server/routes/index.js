74cf2da102ee10cce29ce6b4fe9ea319a7a12ef1
omertaskn452 <omer_taskn@outlook.com>
2022-10-14 20:16:11 +0300

    Mekanbul v1.1

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var ctrlMekanlar=require('../controllers/mekanlar');
+var ctrlDigerleri=require('../controllers/digerleri');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/', ctrlMekanlar.anaSayfa);
+router.get('/mekan', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
+router.get('/hakkinda', ctrlDigerleri.hakkinda);
 
 module.exports = router;
