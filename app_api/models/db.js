5c9ba6a1e26c4c629fa1d1808df81ac4d6d939bc
omertaskn452 <omer_taskn@outlook.com>
2022-12-04 21:51:17 +0300

    update v1.6.8

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require( 'mongoose' );
 //var dbURI = "mongodb://localhost/mekanbul"
 require("./mekansema");
-var dbURI = 'mongodb+srv://omertaskn452:omertaskn452@mekanbul.lsjeuiv.mongodb.net/?retryWrites=true&w=majority';
+var dbURI = 'mongodb+srv://omertaskn452:omertaskn452@mekanbul.lsjeuiv.mongodb.net/mekanbul?retryWrites=true&w=majority';
 mongoose.connect(dbURI);
 mongoose.connection.on("connected", function (){
     console.log(dbURI + "adresindeki veritabanına  bağlanıldı!\n");
