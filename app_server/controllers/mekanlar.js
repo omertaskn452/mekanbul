7aa3f671f612d085465181d220dddf0b03aac8dd
omertaskn452 <omer_taskn@outlook.com>
2022-12-10 12:57:45 +0300

    update v1.7.2

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var router = express.Router();
 const axios = require("axios");
 
 var apiSecenekleri = {
@@ -19,7 +20,7 @@ var mesafeyiFormatla = (mesafe) => {
   return yeniMesafe + birim;
 };
 
-var router = express.Router();
+
 
 var anaSayfaOlustur = (res, mekanListesi) => {
   var mesaj;
@@ -67,67 +68,75 @@ var hataGoster = (res, hata) => {
 };
 
 
-const anaSayfa = function (req, res) {
-  const requestUrl = apiSecenekleri.sunucu + apiSecenekleri.apiYolu;
-  axios
-    .get(requestUrl, {
-      params: {
-        enlem: req.query.enlem,
-        boylam: req.query.boylam,
-      },
-    })
-    .then((response) => {
-      var i, mekanlar;
-      mekanlar = response.data;
-      for (i = 0; i < mekanlar.length; i++) {
-        mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe);
+const anaSayfa = function(req, res) {
+  axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu,{
+      params:{
+          enlem:req.query.enlem,
+          boylam:req.query.boylam
       }
-      anaSayfaOlustur(res, mekanlar);
-    })
-    .catch((hata) => {
-      anaSayfaOlustur(res, hata);
-    });
-};
+  }).then(function(response){
+      var i,mekanlar;
+      mekanlar=response.data;
+      for (i=0;i<mekanlar.length;i++){
+          mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
 
-const mekanBilgisi = function (req, res, next) {
-  axios
-    .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
-    .then((response) => {
-      req.session.mekanAdi=response.data.ad;
-      detaySayfasiOlustur(res, response.data);
-    })
-    .catch((hata) => {
-      hataGoster(res, hata);
-    });
-};
+      }
+      anaSayfaOlustur(res,mekanlar);
 
-const yorumEkle = function (req, res, next) {
-    var mekanAdi=req.session.mekanAdi;
-    var mekanid = req.params.mekanid;
-    if(!mekanAdi){ 
-        res.redirect("mekanid/"+mekanid);
-    }else
-    res.render('yorumekle',{"baslik":mekanAdi+" mekanına yorum ekle",title: 'Yorum Ekle'});
-};
-
-const yorumumuEkle=function(req,res){
-  var gonderilenYorum,mekanid;
-  mekanid=req.params.mekanid;
-  if(!req.body.adsoyad || !req.body.yorum){
-      res.redirect("/mekan"+mekanid+"/yorum/yeni?hata=evet")
-  }
-  else{
-  gonderilenYorum={
-      yorumYapan:req.body.adsoyad,
-      puan:req.body.puan,
-      yorumMetni:req.body.yorum
-  }
-  axios.post(apiSecenekleri.sunucu+apiSecenekleri.apiYolu+mekanid+"/yorumlar",gonderilenYorum)
-  .then(function(){
-      res.redirect("/mekan/"+mekanid);
+  }).catch(function(hata){
+      anaSayfaOlustur(res,hata);
   });
-}
+
 };
+
+const mekanBilgisi = function(req, res){
+  axios
+      .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+      .then(function(response){
+          req.session.mekanAdi = response.data.ad;
+          detaySayfasiOlustur(res, response.data);
+
+      })
+      .catch(function(hata){
+          hataGoster(res,hata);
+      });
+};
+
+const yorumEkle = function(req, res) {
+  var mekanAdi = req.session.mekanAdi;
+  mekanid = req.params.mekanid;
+  if(!mekanAdi)
+  res.redirect("/mekan/"+ mekanid);
+  else res.render("yorumekle",{
+      baslik: mekanAdi + " mekanına yorum ekle"});
+};
+
+const yorumumuEkle=function(req,res) {
+  var gonderilenYorum, mekanid;
+  mekanid = req.params.mekanid;
+  
+
+  if(!req.body.adsoyad || !req.body.yorum){
+      res.redirect("/mekan/"+ mekanid + "/yorum/yeni?hata=evet");
+
+  }else{
+      gonderilenYorum = {
+          yorumYapan: req.body.adsoyad,
+          puan: parseInt(req.body.puan, 10),
+          yorumMetni: req.body.yorum,
+
+      };
+      axios
+          .post(apiSecenekleri.sunucu + apiSecenekleri.apiYolu+mekanid+"/yorumlar",gonderilenYorum)
+          .then(function(){
+              res.redirect("/mekan/" + mekanid);
+          })
+          .catch(function(hata){
+              hataGoster(req,res,hata);
+          });
+  }
+};
+
 
 module.exports = {
   anaSayfa,
